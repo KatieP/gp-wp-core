3e54882df8ccb77d591a5cadb2f32a5569d8ced8
Katie Patrick <katie.patrick@thegreenpages.com.au>
2013-05-02 12:24:01 +1000

    Fix typo on maps page

--- wordpress/wp-content/plugins/gp-theme/pages/gp-world-map.php
+++ wordpress/wp-content/plugins/gp-theme/pages/gp-world-map.php
@@ -4,7 +4,7 @@
  * Found at www.greenpag.es/world-map OR www.graanrazor.co/world-map
  */
   
-echo '<p>A world map of the world’s environmental issues, projects and events. Click on an icon on the map to learn more.</p>'; 
+echo '<p>A map of the world’s environmental issues, projects and events. Click on an icon on the map to learn more.</p>'; 
 get_google_word_map(); 
 
 ?>
