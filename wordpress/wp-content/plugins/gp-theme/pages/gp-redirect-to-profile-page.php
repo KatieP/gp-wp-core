a06cbd77a077f0bc52aa0feb0d6d9e371fa25b33
Jesse Browne <jb@greenpag.es>
2013-06-18 07:51:32 +1000

    Actually added page for redirect to profile page this time

---
+++ wordpress/wp-content/plugins/gp-theme/pages/gp-redirect-to-profile-page.php
@@ -0,0 +1,11 @@
+<?php 
+	
+	/**
+	 * Redirects logged in user to their own profile page
+	 * Handy for redirecting from other plugins that can't access user variables
+	 * Located at: /forms/redirect-to-profile-page/ 
+	 **/
+
+    redirect_to_users_own_profile_page(); 
+
+?>
