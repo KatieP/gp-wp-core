29e19c78231f5bfcf75e49551e560383cd7ccac2
Jesse Browne <jb@greenpag.es>
2013-07-11 14:05:43 +1000

    Tidied messages printed to screen for users on gp-chargify-upgrade-downgrade-handler

--- wordpress/wp-content/plugins/gp-theme/pages/gp-chargify-upgrade-downgrade-handler.php
+++ wordpress/wp-content/plugins/gp-theme/pages/gp-chargify-upgrade-downgrade-handler.php
@@ -2,15 +2,15 @@
 /**
  * Sends new product data to chargify api and updates user meta for an existing advertiser that
  * upgrades or downgrades to a different plan from their profile page.
- * 
+ *
  *  1. Inserts chargify data from signup into wp_usermeta
  *  2. Explains to client how advertising works
  *  3. Inlcudes gravity form for advertiser to create their first post
- *  
+ *
  *  Authors: Katie Patrick & Jesse Browne
- *  		 kp@greenpag.es
+ *           kp@greenpag.es
  *           jb@greenpag.es
- *           
+ *
  **/
 
 if ( is_user_logged_in() ) {
@@ -22,15 +22,18 @@ if ( is_user_logged_in() ) {
     if ( ($_POST['upgrade']) || ($_POST['downgrade']) ) {
 
         $product_id_key   = 'product_id';
+
+        ?><p>Adjusting your plan right now, please do not exit or reload this page while update in progress ...</p><?php
         
         if ($_POST['upgrade']) {
-            echo 'Upgrading to plan with product_id '. $_POST['upgrade'];
-            $product_id_value = $_POST['upgrade'];
+            $product_id_value =  $_POST['upgrade'];
+            $success_message =   'Your plan has been successfully upgraded, update is now complete.';
         }
-        
+
         if ($_POST['downgrade']) {
-            echo 'Upgrading to plan with product_id '. $_POST['downgrade'];
-            $product_id_value = $_POST['downgrade'];
+            echo 'Downgrading to plan with product_id '. $_POST['downgrade'];
+            $product_id_value =  $_POST['downgrade'];
+            $success_message =   'Your plan has been successfully downgraded, update is now complete.';
         }
 
         $subscription_id =    $current_user->subscription_id;
@@ -46,7 +49,7 @@ if ( is_user_logged_in() ) {
         $chargify_auth =      $chargify_key .':x';
         $chargify_auth_url =  'https://'. $chargify_auth .'green-pages.chargify.com/subscriptions/';
 
-    	$chargify_url = 'https://green-pages.chargify.com/subscriptions/' . $subscription_id .'/migrations.json';        
+        $chargify_url = 'https://green-pages.chargify.com/subscriptions/' . $subscription_id .'/migrations.json';
 
         $ch = curl_init($chargify_auth_url);
 
@@ -62,26 +65,21 @@ if ( is_user_logged_in() ) {
         curl_setopt($ch, CURLOPT_VERBOSE, true);
         curl_setopt($ch, CURLOPT_USERPWD, $chargify_auth);
 
-        $result = curl_exec($ch);
+        $json_result = curl_exec($ch);
 
-        echo '<br />';
-        var_dump($result);           
-        echo '<br />';
-        
-        $result_as_array = json_decode($result, true);
+        $result = json_decode($json_result);
 
-        echo '<br />';
-        var_dump($result_as_array);           
-        echo '<br />';        
-        
-        update_user_meta( $user_id, $product_id_key, $product_id_value );        
-        
+        if ($result->subscription->product->id) {
+            update_user_meta( $user_id, $product_id_key, $product_id_value );
+            echo $success_message;
+        }
+
     }
 
 } else {
-    
+
     ?><p>Not sure how you got here without being logged in, head back to the <a href="<?php echo $site_url; ?>">home page</a>, 
     there's nothing to see here.</p><?php
-    
+
 }
 ?>
