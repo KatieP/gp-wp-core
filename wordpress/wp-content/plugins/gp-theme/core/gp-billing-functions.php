fe714d4aa7d385b669c32d2729a9259c8b9819f4
Jesse Browne <jb@greenpag.es>
2013-08-27 03:43:38 +1000

    Moved more billing functions into gp-billing-functions.php

--- wordpress/wp-content/plugins/gp-theme/core/gp-billing-functions.php
+++ wordpress/wp-content/plugins/gp-theme/core/gp-billing-functions.php
@@ -7,6 +7,349 @@
  * 
  */
 
+function get_billing_history($subscription_id,  $component_id) {
+
+    $chargify_key =       '3FAaEvUO_ksasbblajon';
+	$chargify_auth =      $chargify_key .':x';
+	$chargify_auth_url =  'https://'. $chargify_auth .'green-pages.chargify.com/subscriptions/';
+    $chargify_url =       'https://green-pages.chargify.com/subscriptions/' . $subscription_id . '/components/' . $component_id . '/usages.json';
+    
+    // Chargify api key: 3FAaEvUO_ksasbblajon
+    // http://docs.chargify.com/api-authentication
+
+    $ch = curl_init($chargify_auth_url);
+
+    $array = array();
+ 
+    array_push($array, 'Content-Type: application/json;', 'Accept: application/json;', 'charset=utf-8;');
+
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $array);
+    curl_setopt($ch, CURLOPT_URL, $chargify_url);
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_FRESH_CONNECT, true);
+    curl_setopt($ch, CURLOPT_VERBOSE, true);
+    curl_setopt($ch, CURLOPT_USERPWD, $chargify_auth);
+
+    $json_result = curl_exec($ch);
+    $result = json_decode($json_result);
+    
+    curl_close($ch);
+    return $result;    
+
+}
+
+function theme_profile_billing($profile_pid) {
+    /**
+     * Billing panel on profile page
+     * 
+     * Allows user to upgrade or downgrade cost per click advertising plans
+     * and update credit card details
+     * 
+     * Shows some advertiser history
+     * 
+     * Authors: Katie Patrick & Jesse Browne
+     *          kp@greenpag.es
+     *          jb@greenpag.es
+     **/
+	
+	global $current_user;
+
+	if ( ( $current_user->reg_advertiser == '1' ) || ( get_user_role( array('administrator') ) ) ) {} else { return; }
+	
+	$profile_author =                  get_user_by('slug', $profile_pid);
+	$profile_author_id =               $profile_author->ID;
+    $site_url =                        get_site_url();
+    $user_ID =                         $current_user->ID;
+    $product_id =                      $profile_author->product_id;
+    $subscription_id =                 $profile_author->subscription_id;
+    $budget_status =                   $profile_author->budget_status;
+    $advertiser_signup_time =          $profile_author->adv_signup_time;
+    $chargify_self_service_page_url =  ( !empty($subscription_id) ) ? create_update_payment_url($profile_author) : '';
+    $component_id = 				   get_component_id($product_id);
+    
+    if ( ( ( is_user_logged_in() ) && ( $current_user->ID == $profile_author->ID ) ) || get_user_role( array('administrator') ) ) {} else {return;}
+
+    if (!empty($chargify_self_service_page_url)) {
+        ?><a href="<?php echo $chargify_self_service_page_url; ?>" target="_blank"><h3>Update my credit card details</h3></a><?php    
+    } else {
+        ?>
+        <h3>You currently aren't signed up to a plan with us.</h3>
+        <h3><a href="<?php echo $site_url; ?>/advertisers/">Choose a plan.</a></h3>
+        <p>Doesn't sound right?</p>
+        <p>Send us an email at hello[at]greenpag.es and we'll get to the bottom of it.</p>
+        <?php 
+    }    
+    
+    $plan = get_product_name($product_id);
+
+    if ( !empty($product_id) && !empty($plan) ) {
+	    
+        if ( $budget_status != 'cancelled' ) {
+            ?><h3>You are on the <?php echo $plan; ?></h3><?php
+        } else {
+            ?><h3>You were on the <?php echo $plan; ?>, however your subscription is currently cancelled.</h3><?php   
+        }
+        upgrade_plan($product_id, $budget_status);
+	
+		if (!empty($component_id)) {
+		
+			$history = get_billing_history($subscription_id,  $component_id); 
+			
+			?><h3>Current Subscription History</h3><?php 
+			
+			$i = 0; 
+			foreach ($history as $usage) {
+			    $date =             substr( $usage->usage->created_at, 0, 10 );
+			    $clicks =           $usage->usage->quantity;
+			    $plan_cpc =         get_cost_per_click($product_id);
+			    $cpc =              ( $plan_cpc != NULL ) ? (float) $plan_cpc : (float) 0.0;
+			    $billable =         ( $plan_cpc != NULL ) ? ( (int) $clicks ) * $cpc : (float) 0.0;
+			    $pretty_cpc =       number_format($cpc, 2);
+			    $pretty_billable =  number_format($billable, 2);
+			    $total_billed +=    $billable; 
+    				
+			    if ( $i == 2 ) { ?>
+				    
+				    <table class="author_analytics">
+				        <tr>
+					        <td>Activity Date</td>
+					        <td>Clicks</td>
+					        <td>Cost Per Click</td>
+					        <td>Billing Amount</td>
+				        </tr> <?php 
+			    }
+			        
+    			if ($date == $prev_date) { 
+    			    $sum_clicks +=          $clicks;
+    			    $sum_billable =         ( $plan_cpc != NULL ) ? ( (int) $sum_clicks ) * $cpc : (float) 0.0;
+    			    $sum_pretty_billable =  number_format($sum_billable, 2);
+    			} else { 
+                    if ( !empty($sum_clicks) ) { ?>
+            			<tr>
+            				<td><?php echo $prev_date; ?></td>
+            				<td><?php echo $sum_clicks; ?></td>
+            				<td><?php echo '$'. $pretty_cpc; ?></td>
+            				<td><?php echo '$'. $sum_pretty_billable; ?></td>
+            			</tr><?php
+        			    $sum_clicks =           '';
+        			    $sum_billable =         '';
+        			    $sum_pretty_billable =  '';
+                    } elseif ( !empty( $prev_date ) ) { ?>
+            			<tr>
+            				<td><?php echo $date; ?></td>
+            				<td><?php echo $clicks; ?></td>
+            				<td><?php echo '$'. $pretty_cpc; ?></td>
+            				<td><?php echo '$'. $pretty_billable; ?></td>
+            			</tr><?php
+                    }
+                }
+                    
+                $prev_date = $date;
+                $i++;
+		    }
+			
+		    if ($i >= 2) { ?></table><?php }
+			 	
+            $total_billed = number_format($total_billed, 2); ?>
+            <table class="author_analytics">
+                <tr>
+        	        <td><strong>Total billed:</strong></td>
+        			<td><?php echo '$'.$total_billed; ?></td>
+        		</tr>
+    		</table> <?php
+
+		} elseif ( $product_id == '27023') { ?>
+			<h3>
+			    <p>Why don't you change your subscription to a cost per click plan? 
+		    	You'll be able to create unlimited product posts only pay for the clicks you receive. 
+				Simply choose a plan from the 'upgrade' menu above.</p>
+		    <h3> <?php
+		}	
+		downgrade_plan($product_id, $budget_status);
+	}
+}
+
+function upgrade_plan($product_id, $budget_status) {
+    /**
+     * Show appropriate list of advertising plans for user to upgrade to
+     * Called by theme_profile_billing()
+     * 
+     **/
+    	
+    if ($product_id != '3313297') {} else { return; }
+    $site_url = get_site_url();
+
+    if ( $budget_status != 'cancelled' ) {
+        ?><h3>Upgrade</h3><?php
+        $name = 'upgrade'; 
+    } else {
+        ?><h3>Reactivate</h3><?php
+        $name = 'reactivate';   
+    }
+    
+    ?><form action="<?php echo $site_url; ?>/chargify-upgrade-downgrade-handler/" method="post"><?php
+
+    if ( $name == 'reactivate' ) {
+        switch ($product_id) {
+            case '3313297':	//$499/wk
+    			echo '<select name="'. $name .'">
+    			 		<option value="3313297"> &nbsp&nbsp&nbsp $449/week plan &nbsp&nbsp&nbsp </option>
+    		  		</select>';			
+    		  	break;	
+    		case '3313296':	//$249/wk
+    			echo '<select name="'. $name .'">
+    			 		<option value="3313296">  &nbsp&nbsp&nbsp $249/week plan &nbsp&nbsp&nbsp </option>
+    		  		</select>';			
+    		  	break;	
+    		case '27028': //$99/wk
+    			echo '<select name="'. $name .'">
+    			 		<option value="27028">  &nbsp&nbsp&nbsp $99/week plan &nbsp&nbsp&nbsp </option>
+    		  		</select>';			
+    		  	break;	
+    		case '27029': //$39/wk
+    			echo '<select name="'. $name .'">
+    		     		<option value="27029"> &nbsp&nbsp&nbsp $39/week plan &nbsp&nbsp&nbsp </option>
+    		  		</select>';			
+    		  	break;		
+    		case '3313295': //$12/wk
+    			echo '<select name="'. $name .'">
+    			 		<option value="3313295"> &nbsp&nbsp&nbsp $12/week plan &nbsp&nbsp&nbsp </option>
+    		  		</select>';			
+    		  	break;
+    	}
+		?><input type="submit" value="Confirm Reactivation">
+    	</form>
+    	<div class="clear"></div><?php 
+		return;
+    }
+    
+    switch ($product_id) {
+		case '3313296':	//$249/wk
+			echo '<select name="'. $name .'">
+			 		<option value="3313297"> &nbsp&nbsp&nbsp $499/week plan &nbsp&nbsp&nbsp </option>
+		  		</select>';			
+		  	break;	
+		case '27028': //$99/wk
+			echo '<select name="'. $name .'">
+			 		<option value="3313297">  &nbsp&nbsp&nbsp $499/week plan &nbsp&nbsp&nbsp </option>
+		  	 		<option value="3313296">  &nbsp&nbsp&nbsp $249/week plan &nbsp&nbsp&nbsp </option>
+		  		</select>';			
+		  	break;	
+		case '27029': //$39/wk
+			echo '<select name="'. $name .'">
+			 		<option value="3313297"> &nbsp&nbsp&nbsp $499/week plan &nbsp&nbsp&nbsp </option>
+		  	 		<option value="3313296"> &nbsp&nbsp&nbsp $249/week plan &nbsp&nbsp&nbsp  </option>
+		     		<option value="27028"> &nbsp&nbsp&nbsp $99/week plan &nbsp&nbsp&nbsp </option>
+		  		</select>';			
+		  	break;		
+		case '3313295': //$12/wk
+			echo '<select name="'. $name .'">
+			 		<option value="3313297"> &nbsp&nbsp&nbsp $499/week plan &nbsp&nbsp&nbsp </option>
+		  	 		<option value="3313296"> &nbsp&nbsp&nbsp $249/week plan &nbsp&nbsp&nbsp </option>
+		     		<option value="27028"> &nbsp&nbsp&nbsp $99/week plan &nbsp&nbsp&nbsp </option>
+			 		<option value="27029"> &nbsp&nbsp&nbsp $39/week plan &nbsp&nbsp&nbsp </option>
+		  		</select>';			
+		  	break;
+		case '27023': //Directory $39 / month
+			echo '<select name="'. $name .'">
+			 		<option value="3313297"> &nbsp&nbsp&nbsp $499/week plan &nbsp&nbsp&nbsp </option>
+		  	 		<option value="3313296"> &nbsp&nbsp&nbsp $249/week plan &nbsp&nbsp&nbsp </option>
+		     		<option value="27028"> &nbsp&nbsp&nbsp $99/week plan &nbsp&nbsp&nbsp </option>
+			 		<option value="27029"> &nbsp&nbsp&nbsp $39/week plan &nbsp&nbsp&nbsp </option>
+			 		<option value="3313295"> &nbsp&nbsp&nbsp $12/week plan &nbsp&nbsp&nbsp </option>
+		  		</select>';			
+		  	break;	
+	}
+	
+	?><input type="submit" value="Confirm Upgrade">
+	</form>
+	<div class="clear"></div><?php 
+}
+
+
+function downgrade_plan($product_id, $budget_status) {
+    /**
+     * Show appropriate list of advertising plans for user to downgrade to
+     * Called by theme_profile_billing()
+     * 
+     **/
+
+    if ($budget_status == 'cancelled') { return; }	
+    $site_url = get_site_url();
+    
+    ?><h3>Downgrade</h3>
+    <form action="<?php echo $site_url; ?>/chargify-upgrade-downgrade-handler/" method="post">
+    <?php
+    
+    switch ($product_id) {
+		case '3313297':	//$499/wk
+			echo '<select name="downgrade">
+			 		<option value="3313296"> &nbsp&nbsp&nbsp $249/week plan &nbsp&nbsp&nbsp </option>
+		     		<option value="27028"> &nbsp&nbsp&nbsp $99/week plan &nbsp&nbsp&nbsp </option>
+			 		<option value="27029"> &nbsp&nbsp&nbsp $39/week plan &nbsp&nbsp&nbsp </option>
+			 		<option value="3313295"> &nbsp&nbsp&nbsp $12/week plan &nbsp&nbsp&nbsp </option>
+			 		<option value="cancel"> &nbsp&nbsp&nbsp Cancel Advertising &nbsp&nbsp&nbsp </option>
+		  		</select>';			
+		  	break;	
+		case '3313296': //$249/wk
+			echo '<select name="downgrade">
+			 		<option value="27028"> &nbsp&nbsp&nbsp $99/week plan &nbsp&nbsp&nbsp </option>
+			 		<option value="27029"> &nbsp&nbsp&nbsp $39/week plan &nbsp&nbsp&nbsp </option>
+			 		<option value="3313295"> &nbsp&nbsp&nbsp $12/week plan &nbsp&nbsp&nbsp </option>
+			 		<option value="cancel"> &nbsp&nbsp&nbsp Cancel Advertising &nbsp&nbsp&nbsp </option>
+		  		</select>';			
+		  	break;
+		 case '27028': //$99/wk
+			echo '<select name="downgrade">
+			 		<option value="27029"> &nbsp&nbsp&nbsp $39/week plan &nbsp&nbsp&nbsp </option>
+			 		<option value="3313295"> &nbsp&nbsp&nbsp $12/week plan &nbsp&nbsp&nbsp </option>
+			 		<option value="cancel"> &nbsp&nbsp&nbsp Cancel Advertising &nbsp&nbsp&nbsp </option>
+		  		</select>';			
+		  	break;	
+		case '27029': //$39/wk
+			echo '<select name="downgrade">
+			 		<option value="3313295"> &nbsp&nbsp&nbsp $12/week plan &nbsp&nbsp&nbsp </option>
+			 		<option value="cancel"> &nbsp&nbsp&nbsp Cancel Advertising &nbsp&nbsp&nbsp </option>
+		  		</select>';			
+		  	break;		
+		case '3313295': //$12/wk
+			echo '<select name="downgrade">
+			 		<option value="cancel"> &nbsp&nbsp&nbsp Cancel Advertising &nbsp&nbsp&nbsp </option>
+		  		</select>';			
+		  	break;			
+		case '27023': //Directory $39 / month
+			echo '<select name="downgrade">
+					<option value="3313295"> &nbsp&nbsp&nbsp $12/week plan &nbsp&nbsp&nbsp </option>
+			 		<option value="cancel"> &nbsp&nbsp&nbsp Cancel Advertising &nbsp&nbsp&nbsp </option>
+		  		</select>';			
+		  	break;	
+	}
+	?><input type="submit" value="Confirm Downgrade">
+	</form>
+	<div class="clear"></div><?php 
+}
+
+function create_update_payment_url($profile_author) {
+    /**
+     *  This creates the SHA1 token at the end of the url for the update_payment, requires first 10 digits 
+     *  $token = sha1 ("update_payment--3364787--OG6AQ4YsCTh2lRfEP6p3");
+     *
+     *  Author: Katie Patrick 
+     *          kp@greenpag.es
+     **/
+	
+	$site_key =            'OG6AQ4YsCTh2lRfEP6p3'; // Site Shared Key:
+	$subscription_id =     $profile_author->subscription_id;
+	$sha_input =           'update_payment--'. $subscription_id .'--'. $site_key;
+	$token =               sha1($sha_input);
+	$token_10 =            substr($token, 0, 10);  // returns first 10 digits of sha
+
+	$update_payment_url =  'https://green-pages.chargify.com/update_payment/' . $subscription_id.'/'. $token_10;
+
+	return $update_payment_url;
+}
+
 function get_product_name($product_id) {
     /**
      * Map of product_id to names of plans
